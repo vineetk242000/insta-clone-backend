e559a3eb50c5e9ee4315ac74ce7daa723009006e
Vineet Kumar <vineetkumar24200@gmail.com>
2021-02-23 12:11:53 +0530

    fixed mongodb connection issue

--- utility/db.js
+++ utility/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 
 const connect = async function () {
-    const uri = "mongodb+srv://vineetk242000:vineet001@cluster0.euiog.mongodb.net/User?retryWrites=true&w=majority"; // Will return DB URI 
+    const uri = "Your Mongodb Url"; // Will return DB URI 
     console.log(`Connecting to DB - uri: ${uri}`);
     return mongoose.connect(uri, {useNewUrlParser: true,useFindAndModify:false});
   };
